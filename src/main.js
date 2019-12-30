dded54fec94d966f1d16366243d9b6aee4ade0e9
Enrique Franco <luisfrancoz1@gmail.com>
2019-12-30 13:41:15 -0500

    Ref Inline components

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ Vue.config.productionTip = false
 
 // Componentes 
 
-var componente1 = Vue.extend({
-  template: '<h1>Hola - Introduction a componentes</h1>'
-})
+// var componente1 = Vue.extend({
+//   template: '<h1>Hola - Introduction a componentes</h1>'
+// })
 
 var componente2 = Vue.extend({
   template: '<h3>Listado de Framworks JS</h3>'
@@ -17,7 +17,13 @@ var componente3 = Vue.extend({
   template: '<ul><li>VueJS</li><li>Angular</li><li>React</li></ul>'
 })
 
-Vue.component('componente-saludo', componente1)
+Vue.component('componente-saludo', {
+  methods: {
+    display(){
+      console.log('Hola Mundo');
+    }
+  }
+})
 
 Vue.component('componente-titulo', componente2)
 
