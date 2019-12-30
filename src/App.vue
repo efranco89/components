d154b258d6528e431b0fbe78d5e1c4414437c688
Enrique Franco <luisfrancoz1@gmail.com>
2019-12-30 14:26:37 -0500

    Ref Single Vue File Component

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,15 @@
 </template>
 
 <script>
-  import HelloWorld from './components/HelloWorld'
   import PostExample from './components/Posts'
   export default {
     name: 'App',
     components:{
-      HelloWorld,
       PostExample
     }
   }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 </style>
--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-    <p> <b> HOLA </b></p>
+  <div class="container">
+    <table class="table table-dark">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Titulo</th>
+          <th scope="col">Body</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="post in posts" :key="post">
+          <td>{{ post.id }} </td>
+          <td>{{ post.titulo }}  </td>
+          <td>{{ post.body }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -10,6 +25,12 @@
     name: 'PostExample',
     data(){
       return {
+        posts: [
+          { id: 1, titulo: 'Post 1', body: "This is sparta" },
+          { id: 2, titulo: 'Post 2', body: "This is athens" },
+          { id: 3, titulo: 'Post 3', body: "This is santorini" },
+          { id: 4, titulo: 'Post 4', body: "This is athos" }
+        ]
 
       }
     }
